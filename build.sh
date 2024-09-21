c9a8b7a3660994a7a5f34ca88454c67f3ac2294d
Sielb2 <siel3@yahoo.com>
2024-09-21 21:52:48 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-sudo apt update
 echo "useless notice that the build is starting!!!"
-cd
 nasm -felf64 home.asm
 ld home.o -o home
 echo "build completed"
-
+cd ..
 counter=1
 while [ $counter -le 5 ]
 do
-        touch "file$counter.txt"
-        ((counter++))
+touch "file$counter.txt"
+((counter++))
 done
 echo "Created 5 starting files for the file system"
